3f22e7830720d3bf8525ffedd46255419912217a
Carlos Daniel Vilaseca <carlosd.vilaseca@avangenio.com>
2024-01-08 02:57:23 -0500

    feat: version and author

---
+++ src/lib/components/Version.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { PUBLIC_VERSION } from '$env/static/public';
+</script>
+
+<div class="bg-slate-800 rounded-md px-3 py-1">
+	<span class="text-md text-slate-600">v{PUBLIC_VERSION}</span>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.pcss';
+	import Github from '$lib/components/icons/Github.svelte';
+	import Version from '$lib/components/Version.svelte';
 </script>
 
 <ModeWatcher defaultMode="dark" track={false} />
 <slot />
+
+<div class="fixed bottom-5 right-5 flex gap-4 items-center text-base">
+	By:
+	<a
+		href="https://github.com/carlos-err406"
+		target="_blank"
+		rel="noreferrer"
+		class="hover:underline"
+	>
+		Carlos Daniel Vilaseca Illnait (Carlos-err406)
+	</a>
+	<Github />
+</div>
+
+<div class="fixed bottom-5 left-5">
+	<Version />
+</div>
